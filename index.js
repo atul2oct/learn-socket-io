044b4b9ce2a3dce7ce997f9c05feccef22df2994
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2025-08-25 16:48:31 +0530

    .

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+// 1. Import
 import express from "express";
 import http from "http";
-import { dirname, join } from "path";
+import { dirname, join } from "node:path";
 import { Server } from "socket.io";
-import { fileURLToPath } from "url";
+import { fileURLToPath } from "node:url";
+
+
+// 2. Instances
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// 3. Serving html file
+const __dirname = dirname(fileURLToPath(import.meta.url));
+// console.log(join(__dirname,'index.html'));
+app.get('/',(req,res)=> res.sendFile(join(__dirname,'index.html')));
+// 4. Define a coneection event handler
+// 5. Start the server
+const PORT = 3000;
+server.listen(PORT,()=>console.log(`Server is up and running or Port No. ${PORT}`));
